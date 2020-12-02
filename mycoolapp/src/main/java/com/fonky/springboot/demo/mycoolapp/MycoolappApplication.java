7490cc5254a39533e66c17afb69cda66f63e4fbd
andresfonky <andresfonquernie@gmail.com>
2020-12-02 17:36:30 +0100

    Spring boot App - Starting

---
+++ mycoolapp/src/main/java/com/fonky/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,13 @@
+package com.fonky.springboot.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappApplication.class, args);
+		System.out.println("Running... ");
+	}
+}

bc5e7eaa597befaff9f41bc976efd53751609277
andresfonky <andresfonquernie@gmail.com>
2020-12-07 16:50:17 +0100

    Udemy - Spring Boot Tutorial Actuator Properties

---
+++ spring-boot-demo/src/main/java/com/fonky/springboot/demo/mycoolapp/controller/FunRestController.java
@@ -0,0 +1,40 @@
+package com.fonky.springboot.demo.mycoolapp.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+	// inject properties 
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "HelloWorld! MyCoolApp and time is " + LocalDateTime.now();
+	}
+	
+	@GetMapping("/workout")
+	public String getDailyWorkOut() {
+		return "Run a hard 5k!";
+	}
+	
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day.";
+	}
+	
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + this.coachName + ", Team: " + this.teamName;
+	}
+
+}
